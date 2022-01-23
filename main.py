49dd918f43975dc344433a5e8aa177e2fb09fe8e
parvshah010806 <nimish.a116.68@gmail.com>
2022-01-23 23:07:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
+from telegram.ext import Updater
+updater = Updater(token='TOKEN', use_context=True)
+
+dispatcher = updater.dispatcher
+
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
 from telegram import Update
 from telegram.ext import CallbackContext
-from telegram.ext import Updater
-import logging
-updater = Updater(token='5227297003:AAEwSnVl-aCGUYGbj92L4m1z6GWTh3ggGa8', use_context=True)
 
 def start(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                        level=logging.INFO)
-
+    from telegram.ext import CommandHandler
     start_handler = CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
+
     updater.start_polling()
