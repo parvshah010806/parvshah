a7aed532c99d55cee3788126ad27ccd938846fef
parvshah010806 <nimish.a116.68@gmail.com>
2022-01-23 23:49:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,6 @@ import logging
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
-# Enable logging
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
-
-logger = logging.getLogger(__name__)
-
-
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
-# Best practice would be to replace context with an underscore,
-# since context is an unused local variable.
-# This being an example and not having context present confusing beginners,
-# we decided to have it present as context.
 def start(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
  
@@ -26,8 +12,3 @@ dispatcher = updater.dispatcher
 
 
 updater.start_polling()
-updater.idle()
-
-
-if __name__ == '__main__':
-    main()
