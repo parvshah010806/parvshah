3ddc87d3ca672c35d0bedda741bcddd04d48f786
parvshah010806 <nimish.a116.68@gmail.com>
2022-01-25 16:00:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-import smtplib 
-from email.message import EmailMessage
-connect()
-email = EmailMessage() ## Creating a object for EmailMessage
-email['from'] = 'parv'   ## Person who is sending
-email['to'] = 'nimisha11668@gmail.com'       ## Whom we are sending
-email['subject'] = 'xyz subject'  ## Subject of email
-email.set_content("Xyz content of email") ## content of email
-with smtplib.SMTP(host='smtp.gmail.com',port=587)as smtp:     
-## sending request to server 
-    
-    smtp.ehlo()          ## server object
-smtp.starttls()      ## used to send data between server and client
-smtop.login("djdnensidoclcmxn@gmail.com","Easyfreezy@456") ## login id and password of gmail
-smtp.send_message(email)   ## Sending email
-print("email sent")    ## Printing success message
+import smtplib, ssl
+
+port = 587  # For starttls
+smtp_server = "smtp.gmail.com"
+sender_email = "djdnensidoclcmxn@gmail.com"
+receiver_email = "nimisha11668@gmail.com"
+password = input("Easyfreezy@456:")
+message = """\
+Subject: Hi there
+
+This message is sent from Python."""
+
+context = ssl.create_default_context()
+with smtplib.SMTP(smtp_server, port) as server:
+    server.ehlo()  # Can be omitted
+    server.starttls(context=context)
+    server.ehlo()  # Can be omitted
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
